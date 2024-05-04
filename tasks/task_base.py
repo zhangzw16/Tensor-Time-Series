a82c6cae09862f96004b0558190c09899e870dd6
ChongKakam <zhuangjx23@mails.tsinghua.edu.cn>
2024-05-04 18:03:47 +0800

    add TensorTask

--- tasks/task_base.py
+++ tasks/task_base.py
@@ -1,27 +1,32 @@
 
 import os
+import math
 import torch
 
 class TaskBase:
-    def __init__(self, configs) -> None:
+    def __init__(self, configs:dict={}) -> None:
         self.configs = configs
-        self.device = configs['device']
-    
-    def ensure_work_dir(self):
+        self.best_valid_loss =  math.inf
+        self.early_stop_max = configs['early_stop_max']
+        self.early_stop_cnt = 0
+    def ensure_work_dir(self, path:str):
         raise NotImplemented
-    
-    def get_data(self):
-        raise NotImplemented
-    
-    def train(self):
-        pass
 
-    def valid(self):
+    def early_stop(self, valid_loss)->bool:
+        # check loss and update
+        if valid_loss < self.best_valid_loss:
+            self.best_valid_loss = valid_loss
+            self.early_stop_cnt = 0
+        else:
+            self.early_stop_cnt += 1
+        # check cnt and return stop flag
+        if self.early_stop_cnt > self.early_stop_max:
+            return True
+        else:
+            return False
+        
+    def train(self):
         pass
 
     def test(self):
         pass
-
-    def early_stop(self):
-        pass
-        
--- tasks/tensor_task.py
+++ tasks/tensor_task.py
@@ -1,33 +1,108 @@
 import os
+import yaml
 import torch
 import numpy as np
 from tasks.task_base import TaskBase
+from models import ModelManager
 from models.model_base import TensorModelBase
+from datasets.dataset import TTS_Dataset
+from datasets.dataloader import TTS_DataLoader
+from utils.evaluation import Evaluator
+
 
 class TensorTask(TaskBase):
-    def __init__(self, configs) -> None:
+    def __init__(self, configs:dict={}) -> None:
         super().__init__(configs)
-        self.model = TensorModelBase()
-        self.mode = 'train'
-        self.model.set_device('cuda')
+        # load configuration
+        self.seed = configs['seed']
+        self.device = configs['task_device']
+        self.log_dir = configs['log_dir']
+        self.model_save_dir = configs['model_save_dir']
+        self.batch_size = configs['batch_size']
+        self.max_epoch = configs['max_epoch']
+        self.early_stop_max = configs['early_stop_max']
+        self.early_stop_cnt = 0
 
-    def get_data(self):
-        return super().get_data()
+        self.pkl_path = configs['dataset_pkl']
+        self.his_len = configs['his_len']
+        self.pred_len = configs['pred_len']
+
+        # prepare for model
+        model_manager = ModelManager()
+        self.model_name = configs['model_name']
+        model_configs = {}
+        self.model = model_manager.get_model_class(self.model_name)(model_configs)
+        self.model.set_device(self.device)
+
+        # prepare for dataset
+        self.dataset = TTS_Dataset(self.pkl_path, 
+                                   his_len=self.his_len, pred_len=self.pred_len ,test_ratio=0.1, valid_ratio=0.1, seed=2024)
+        self.trainloader = TTS_DataLoader(self.dataset, 'train', batch_size=16, drop_last=False)
+        self.validloader = TTS_DataLoader(self.dataset, 'valid', batch_size=16, drop_last=False)
+        self.testloader = TTS_DataLoader(self.dataset, 'test', batch_size=1, drop_last=False)
+        
+        # prepare for evaluation
+        self.eval_verbose  = configs['evaluator_verbose']
+        self.metrics_list  = configs['metrics_list']
+        self.metrics_thres = configs['metrics_thres']
+        self.evaluator = Evaluator(self.metrics_list, self.metrics_thres)
 
     def train(self):
         for i in range(self.max_epoch):
-            self.model.train()
-            for () in self.train_loader:
-                self.model.forward()
-                print('TODO:...')
-        # log information
-        # ...
-    
-    def early_stop(self):
-        return super().early_stop()
+            epoch_mean_train_loss = self.epoch_train()
+            epoch_mean_valid_loss, valid_result = self.epoch_valid()
+            early_stop_flag = self.early_stop(epoch_mean_valid_loss)
+            print(f"epoch: {i}, mean_train_loss: {epoch_mean_train_loss:.3f}, mean_valid_loss:{epoch_mean_valid_loss:.3f}")
+            if early_stop_flag:
+                # TODO: save model ...
+                break
+        # TODO: show training summary
+        print('training finished...')
 
-    def valid(self):
-        return super().valid()
+    def epoch_train(self):
+        self.model.train()
+        loss_list = []
+        for seq in self.trainloader.get_batch(separate=False):
+            seq = seq.to(self.device)
+            pred, truth = self.model.forward(seq)
+            epoch_train_loss = self.model.get_loss(pred, truth)
+            self.model.backward(epoch_train_loss)
+            loss_list.append(epoch_train_loss.item())
+        mean_loss = sum(loss_list)/len(loss_list)
+        return mean_loss
+    
+    def epoch_valid(self):
+        self.model.eval()
+        loss_list = []
+        pred_list = []
+        truth_list = []
+        with torch.no_grad():
+            for seq in self.validloader.get_batch(separate=False):
+                seq = seq.to(self.device)
+                pred, truth = self.model.forward(seq)
+                epoch_valid_loss = self.model.get_loss(pred, truth)
+                loss_list.append(epoch_valid_loss.item())
+                pred = pred.cpu().numpy()
+                truth = truth.cpu().numpy()
+                pred_list.append(pred)
+                truth_list.append(truth)
+        mean_loss = sum(loss_list)/len(loss_list)
+        pred = np.array(pred_list).squeeze()
+        truth = np.array(truth_list).squeeze()
+        result = self.evaluator.eval(pred, truth, verbose=self.eval_verbose)
+        return mean_loss, result
 
     def test(self):
-        return super().test()
+        self.model.eval()
+        pred_list = []
+        truth_list = []
+        with torch.no_grad():
+            for seq in self.testloader.get_batch(separate=False):
+                seq = seq.to(self.device)
+                pred, truth = self.model.forward(seq)
+                pred_list.append(pred)
+                truth_list.append(truth)
+        pred = np.array(pred_list)
+        truth = np.array(truth_list)
+        result = self.evaluator.eval(pred, truth, verbose=self.eval_verbose)
+        return result
