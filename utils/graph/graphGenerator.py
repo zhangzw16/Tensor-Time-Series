8ac55ef3d9a0935fe35fe6f2b85f512977582fe0
ChongKakam <zhuangjx23@mails.tsinghua.edu.cn>
2024-05-08 14:44:33 +0800

    add logger & graph

---
+++ utils/graph/graphGenerator.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class GraphGenerator:
+    def __init__(self) -> None:
+        pass
+    def gen_adjacency_matrix(self):
+        pass
+
---
+++ utils/logger/Logger.py
@@ -0,0 +1,67 @@
+import wandb
+import yaml
+import os
+
+class LoggerBase:
+    def __init__(self, dir:str, project:str, name:str) -> None:
+        pass
+
+    def init(self):
+        pass
+
+    def log(self, info:dict):
+        pass
+
+    def close(self):
+        pass
+
+class Logger_none(LoggerBase):
+    def __init__(self, dir: str, project: str, name: str) -> None:
+        super().__init__(dir, project, name)
+        self.log_dir = dir
+        self.project = project
+        self.name = name
+    def init(self):
+        print(f'logger-none init:')
+        print(f"project: {self.project}")
+        print(f"name: {self.name}")
+
+    def log(self, info: dict):
+        for i in info:
+            print(f'{i}: {info[i]:.3f}')
+
+    def close(self):
+        print('Logger_none: Goodbye...')
+
+class Logger_wandb(LoggerBase):
+    def __init__(self, dir:str, project:str, name:str) -> None:
+        super().__init__(dir, project, name)
+        self.log_dir = os.path.join(dir, 'wandb_log')
+        self.project_name = project
+        self.run_name = name
+
+    def init(self):
+        self.logger =wandb.init(
+            project=self.project_name, name=self.run_name,
+            config=self.configs, dir=self.log_dir)
+    
+    def log(self, info: dict):
+        if self.logger is not None:
+            self.logger.log(info)
+    
+    def close(self):
+        if self.logger is not None:
+            self.logger.finish()
+
+class LoggerManager:
+    def __init__(self) -> None:
+        self.log_map = {
+            'none': Logger_none,
+            'wandb': Logger_wandb,
+        }
+
+    def init_logger(self, logger_name:str, dir:str, project:str, name:str):
+        logger = self.log_map[logger_name]
+        logger = logger(dir=dir, project=project, name=name)
+        return logger
+    
