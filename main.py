20c6e2864a0b70af487c9ce8938e43ee1d5e0b88
ChongKaKam <zhuangjx23@mails@tsinghua.edu.cn>
2024-08-06 02:10:37 +0800

    V3

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import argparse
 from tasks.task_manager import TaskManager
 
 if __name__=='__main__':
     # set default values
     output_dir = '/home/zhuangjiaxin/workspace/TensorTSL/Tensor-Time-Series/output/'
-    dataset_name = 'JONAS_NYC_taxi'
-    model_name = 'TimesNet'
+    dataset_name = 'nasdaq100'
+    model_name = 'PatchTST'
 
     # task manager
     manager = TaskManager(project_name='test',output_dir=output_dir)
