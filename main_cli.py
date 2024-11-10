ec80d156d636b7f4e6e6d6c5d9ca76501ff0763c
ChongKaKam <zhuangjx23@mails@tsinghua.edu.cn>
2024-11-10 20:37:22 +0800

    update

--- main_cli.py
+++ main_cli.py
@@ -133,7 +133,8 @@ if __name__=='__main__':
     elif basic_config['mode'] == 'test':
         if basic_config['model_path'] == '':
             raise ValueError("In test mode, model_path should not be empty.")
-
+    if basic_config['model_name'] == 'GraphWaveNet':
+        basic_config['batch_size'] = 8
     # [debug] echo configs 
     # for k, v in basic_config.items():
     #     print(f"{k}: {v}")
